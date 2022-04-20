342d1fc85f2e1265d1f697ea9841fa0a6b093916
Hector Nava <hector.r.nava44@gmail.com>
2022-04-20 07:59:41 -0600

    Updated auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,11 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -18,7 +14,6 @@ module.exports = {
       return req;
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -26,7 +21,6 @@ module.exports = {
       console.log('Invalid token');
     }
 
-    // send to next endpoint
     return req;
   },
   signToken: function ({ username, email, _id }) {
