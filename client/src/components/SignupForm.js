b808bd2f3679ee753b9129d7df6d58dc258d20d6
Hector Nava <hector.r.nava44@gmail.com>
2022-04-20 19:24:38 -0600

    Added signup graphql call

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -32,15 +32,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await createUser({
+        variables: { ...userFormData }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.createUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
